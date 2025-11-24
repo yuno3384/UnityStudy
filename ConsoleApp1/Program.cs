ed9183a476a6de202ccd31d940c0f754d50f652b
yuno3384 <86882106+yuno3384@users.noreply.github.com>
2025-11-24 16:33:44 +0900

    Time Table Program move

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // 구구단 출력 프로그램
+            // 앞자리는 2부터 뒷자리는 1부터
+            for (int i = 2; i < 10; i++)
+            {
+                Console.WriteLine($"{i}단");
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i}*{j} = {i * j}");
+                }
+            }
         }
     }
 }
