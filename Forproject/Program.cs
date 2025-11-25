5234f0edb04b91cd61e6a3629f9b4e51be02667f
yuno3384 <86882106+yuno3384@users.noreply.github.com>
2025-11-25 10:16:25 +0900

    For-break/continue,Method

---
+++ Forproject/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace Forproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 구구단 출력 프로그램
+            // 앞자리는 2부터 뒷자리는 1부터
+            /*
+             2단
+            2 * 1 = 2
+            2 * 2 = 4
+            .
+            .
+            .
+            9단
+            .
+            .
+            .
+            9 * 8 = 72
+            9 * 9 = 81
+             */
+            //for (int i = 2; i < 10; i++)
+            //{
+            //    Console.WriteLine($"{i}단");
+            //    for (int j = 1; j < 10; j++)
+            //    {
+            //        Console.WriteLine($"{i}*{j} = {i * j}");
+            //    }
+            //}
+
+            // 아직도 중소기업에서는 별짓기가 나온다
+            /*
+                *
+                * *
+                * * *
+                * * * *
+                * * * * *
+                사고 과정) i 안의 j가 반복될 때 j가 *을 출력하는 방식
+                
+             
+            */
+            //Console.Write("*");
+            //Console.WriteLine("**"); // > 이거말고!!
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("-----------------------");
+            /*
+                * * * * *
+                * * * * 
+                * * *
+                * * 
+                * 
+             
+            */
+
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+            // 숙제 하나 더
+            // 1에서 10까지 출력하는데 
+            // 1은 홀수 입니다
+            // 2는 짝수 입니다
+            for (int i = 1; i < 11; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}는 짝수입니다.");
+                }
+                else if (i % 2 == 1)
+                {
+                    Console.WriteLine($"{i}는 홀수입니다.");
+                }
+            }
+
+            //오른쪽 정렬 별찍기
+            /*
+              * * * * * // 1 -> 5
+                * * * * // 2 -> 4 
+                  * * * // 3 -> 3
+                    * * // 4 -> 2
+                      * // 5 -> 1
+             */
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write("q");
+                }
+                for (int j = 6; j > i; j--)
+                {
+                    Console.Write("0");
+                }
+                Console.WriteLine("*");
+            }
+            /*
+                        *
+                      * *
+                    * * *
+                  * * * *
+                * * * * *
+             */
+            // 게임수학을 하면 좋다 > 다크쏘울에서 뒤에 몰래 가서 잡는 것 > 그것을 구현하려면 외접 내접을 해야...
+            // 방향벡터 A를 B로 갈때 d를 구할 때 > 노멀라이즈
+        }
+    }
+}
--- UnityStudy/Program.cs
+++ UnityStudy/Program.cs
@@ -2,117 +2,89 @@
 {
     class Program
     {
-        static void Main()
+
+        static void Output() //함수를 생성 > Output함수는 뭔가를 넣지는 않지만
         {
-            // 구구단 출력 프로그램
-            // 앞자리는 2부터 뒷자리는 1부터
-            /*
-             2단
-            2 * 1 = 2
-            2 * 2 = 4
-            .
-            .
-            .
-            9단
-            .
-            .
-            .
-            9 * 8 = 72
-            9 * 9 = 81
-             */
-            //for (int i = 2; i < 10; i++)
-            //{
-            //    Console.WriteLine($"{i}단");
-            //    for (int j = 1; j < 10; j++)
-            //    {
-            //        Console.WriteLine($"{i}*{j} = {i * j}");
-            //    }
-            //}
+            Console.WriteLine("hello World"); //Hello World를 출력시켜줄거야 > 반환은 없어
+        }
 
-            // 아직도 중소기업에서는 별짓기가 나온다
-            /*
-                *
-                * *
-                * * *
-                * * * *
-                * * * * *
-                사고 과정) i 안의 j가 반복될 때 j가 *을 출력하는 방식
+        static void Main(string[] args)
+        {
+            //for문의 break와 continue
+            // switch의 구조 - case / break / default 와 같이
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i); // 1 2 3 4 5> 왜냐? 조건문 보다 먼저이므로 먼저 출력
+                // 3이 될때 멈추게 하고 싶어 > 조건문 + break 
+                //if(i == 3)
+                //{
+                //    break;// > 반복문을 중단하고 빠져나와 주세요 > 자신과 가장 가까이 있는 스코프를 탈출
+                //}
+                if (i == 2)
+                {
+                    continue; // > 반복문에서 이번 회차만 건너뛰어주세요
+                    //Console.WriteLine(""); // 이 아래 구문은 작동안됨
+                }
+                Console.WriteLine(i); // 1 3 4 5 > 왜냐? 조건문 보다 나중이므로 조건문 조건이 성립하여 제외
+
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= 5; j++)
+                {
+                    if (i == 2)
+                    {
+                        break;  // > 반복문을 중단하고 빠져나와 주세요 > 자신과 가장 가까이 있는 스코프를 탈출
+                    }
+                }
                 
+                Console.WriteLine(i); // 1 3 4 5 > 왜냐? 조건문 보다 나중이므로 조건문 조건이 성립하여 제외
+
+            }
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    while (true)
+                    {
+                        if (i == 2)
+                        {
+                            break; // > 반복문을 중단하고 빠져나와 주세요 > 자신과 가장 가까이 있는 스코프를 탈출
+                        }
+                    }
+                    
+                }
+
+                Console.WriteLine(i); // 1 3 4 5 > 왜냐? 조건문 보다 나중이므로 조건문 조건이 성립하여 제외
+
+            }
+            // break; // > 반복문을 중단하고 빠져나와 주세요 > 자신과 가장 가까이 있는 스코프를 탈출 
+            // 즉 switch문의 구동은 => case 일치 > 일치 시 실행코드를 작동 > break로 switch문 스코프를 탈출하며 종료 > 다음 코드 
+
+            // 함수 (=매소드, 프로시져)
+            // 개념적으로는 코드를 재사용할 수 있도록 하나의 단위로 묶는 거
+            // 만일 같은 것이 계속 반복해야 한다면 > 차라리 틀(거푸집)이 있으면 그게 편하겠다 
+            // static void Main() > Main도 함수다
+            // 반환형식(타입) 함수명(매개변수: 함수에 집어넣을 입력값, 여러개 가능하다)
+            //  Console.WriteLine(" "); / Int.Parse() / void Main() > 끝에 괄호가 붙어있는 것은 다 함수다
+            // 한정자 : static / public / private
+            /*
+                함수의 주요 모양 > 어떤 값을 넣으면 어떠한 행위가 일어나서 어떤 값이 나온다.
+                한정자 반환형식(타입) 함수명(매개변수)
+                {
              
-            */
-            //Console.Write("*");
-            //Console.WriteLine("**"); // > 이거말고!!
-            for (int i = 1; i < 7; i++)
-            {
-                for (int j = 1; j < i; j++)
-                {
-                    Console.Write("*");
                 }
-                Console.WriteLine(" ");
-            }
-            Console.WriteLine("-----------------------");
-            /*
-                * * * * *
-                * * * * 
-                * * *
-                * * 
-                * 
              
-            */
-
-            for (int i = 1; i < 6; i++)
-            {
-                for (int j = 6; j > i; j--)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine(" ");
-            }
-
-            // 숙제 하나 더
-            // 1에서 10까지 출력하는데 
-            // 1은 홀수 입니다
-            // 2는 짝수 입니다
-            for (int i = 1; i < 11; i++)
-            {
-                if(i % 2 == 0)
-                {
-                    Console.WriteLine($"{i}는 짝수입니다.");
-                }
-                else if(i % 2 == 1){
-                    Console.WriteLine($"{i}는 홀수입니다.");
-                }
-            }
-
-            //오른쪽 정렬 별찍기
-            /*
-              * * * * * // 1 -> 5
-                * * * * // 2 -> 4 
-                  * * * // 3 -> 3
-                    * * // 4 -> 2
-                      * // 5 -> 1
              */
-            for (int i = 1; i < 6; i++)
-            {
-                for (int j = 6 ; j > i; j--)
-                {
-                  Console.Write("q");
-                }
-                for (int j = 6; j > i; j--)
-                {
-                    Console.Write("0");
-                }
-                Console.WriteLine("*");
-            }
-            /*
-                        *
-                      * *
-                    * * *
-                  * * * *
-                * * * * *
-             */
-            // 게임수학을 하면 좋다 > 다크쏘울에서 뒤에 몰래 가서 잡는 것 > 그것을 구현하려면 외접 내접을 해야...
-            // 방향벡터 A를 B로 갈때 d를 구할 때 > 노멀라이즈
+
+            Output(); // 함수를 호출
+
+            // for문을 안쓰고 Hello World를 세번 출력 > Hello World 출력을 함수로 해뒀지?
+            Output();
+            Output();
+            Output();
+
+
 
         }
     }
