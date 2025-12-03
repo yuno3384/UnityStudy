835e1ce655ecb209d7c2873027c3c4f5aca5cec4
yuno3384 <86882106+yuno3384@users.noreply.github.com>
2025-12-03 12:32:59 +0900

    feat : 콘솔RPG에 직업 선택시 hp,atk부여 기능 추가

---
+++ JobOOP.cs
@@ -0,0 +1,85 @@
+﻿namespace JobOOP
+{
+    namespace Program
+    {
+        class Program
+        {
+            enum ClassType
+            {
+                None,
+                Knight,
+                Mage,
+                Rogue
+            }
+
+            static ClassType ClassChoice()
+            {
+                Console.WriteLine("직업을 선택하세요!");
+                Console.WriteLine("[1] 기사");
+                Console.WriteLine("[2] 마법사");
+                Console.WriteLine("[3] 도둑");
+
+                ClassType choice = ClassType.None;
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        choice = ClassType.Knight;
+                        break;
+                    case "2":
+                        choice = ClassType.Mage;
+                        break;
+                    case "3":
+                        choice = ClassType.Rogue;
+                        break;
+                }
+
+                return choice;
+            }
+
+            static void CreatePlayer(ClassType choice, out int hp, out int atk)
+            {
+                // 기사(100/10), 마법사(50/15), 도둑(75/12)
+
+                hp = 0;
+                atk = 0;
+                
+                switch (choice)
+                {
+                    case ClassType.Knight:
+                        hp = 100; 
+                        atk = 10;
+                        break;
+                    case ClassType.Mage:
+                        hp = 50;
+                        atk = 15;
+                        break;
+                    case ClassType.Rogue:
+                        hp = 75;
+                        atk = 12;
+                        break;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                ClassType choice = ClassType.None;
+
+                while (true)
+                {
+                    choice = ClassChoice();
+                    if (choice != ClassType.None)
+                    {
+                        // 캐릭터 생성
+                        int hp;
+                        int atk;
+                        CreatePlayer(choice, out hp, out atk);
+
+                        Console.WriteLine($"HP {hp}, ATK {atk}");
+                    }
+                }
+            }
+        }
+    }
+}
